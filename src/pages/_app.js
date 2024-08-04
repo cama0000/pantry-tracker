67cb785a54ac24b0860d8082c3519687b9c1a2df
cama0000 <acama0000@gmail.com>
2024-08-03 22:47:31 -0500

    changed icon and title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,20 @@
 import '../app/globals.css';
 import AuthProvider from '../context/AuthContext';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
 
   return (
+    <>
+    <Head>
+      <title>PantryPal</title>
+      <link rel="icon" href="/images/pantry.png" /> {/* Make sure the favicon.ico is in your public directory */}
+    </Head>
+
     <AuthProvider>
       <Component {...pageProps} />
     </AuthProvider>
+  </>
   );
 }
 
